844451ae02334178874307ec47cd7c419d61dfed
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-04-06 22:19:24 +0530

    homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,15 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { Loader } from "lucide-react";
 
 import {Toaster} from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
+
 
   useEffect(() => {
     checkAuth();
@@ -27,7 +30,7 @@ const App = () => {
   
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
